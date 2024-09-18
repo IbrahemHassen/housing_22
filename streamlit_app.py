63d9cb0b5d0e646a314ad418f58c8cd74cf8b6ce
IbrahemHassen <ibrahemmohye@gmail.com>
2024-09-18 21:13:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,14 @@ if file is not None:
   df= pd.read_csv(file)
   st.write(df)
   num_row=st.slider('chosse num rows',min_value=0,max_value=len(df),step=1)
-name_column=st.multiselect('choose name of columns',df.columns.to_list())
-st.write(df[:num_row][name_column])
+  name_column=st.multiselect('choose name of columns',df.columns.to_list())
+  st.write(df[:num_row][name_column])
 
 if name_column:
   st.write(df[:num_row][name_column])
 else:
   st.write(df[:num_row])
+  
 fig=px.scatter(df,x='population',y='total_rooms') 
 st.plotly_chart(fig)
 

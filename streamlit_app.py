91fa62db9a5ce030e1ad2a2d893f3283ffe44776
IbrahemHassen <ibrahemmohye@gmail.com>
2024-09-18 21:21:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ if file is not None:
   df= pd.read_csv(file)
   st.write(df)
   num_row=st.slider('chosse num rows',min_value=0,max_value=len(df),step=1)
-  name_column=st.multiselect('choose name of columns',df.columns.to_list())
-  st.write(df[:num_row][name_column])
+name_column=st.multiselect('choose name of columns',df.columns.to_list())
+st.write(df[:num_row][name_column])
 
 if name_column:
   st.write(df[:num_row][name_column])
